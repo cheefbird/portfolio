3ef8476c77b0286ee7cb2a1c12b89be9a49e840d
Francis Breidenbach <fbreidenbach@gmail.com>
2024-10-18 19:55:25 -0700

    headlessui kinda sucks for vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,15 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   future: {
-    compatibilityVersion: 4
+    compatibilityVersion: 4,
   },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxt/eslint',
-    '@nuxtjs/color-mode',
-    'nuxt-headlessui'
-  ],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxtjs/color-mode'],
   colorMode: {
-      classSuffix: ''
-    }
-})
+    classSuffix: '',
+  },
+});
